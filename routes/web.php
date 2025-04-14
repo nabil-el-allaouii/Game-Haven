d01d09d7448646ddcc6b513c808132de2092dfe0
nabil-el-allaoui <hassi258456@gmail.com>
2025-04-14 12:29:25 +0100

    GAME-25: Add authentication views and middleware for login and registration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::middleware('AlreadyAuth')->group(function () {
+    Route::get('/login', function () {
+        return view("auth.login");
+    })->name("login");
+    Route::get('/register', function () {
+        return view("auth.register");
+    });
+});
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
