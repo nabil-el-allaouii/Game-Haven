0f56f2efbe1ded2d06dc0589d0808db1b03ac51e
nabil-el-allaoui <hassi258456@gmail.com>
2025-04-14 12:09:20 +0100

    GAME-32: Implement authentication controller and request validation for user registration and login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        Auth::login($user);
+        return redirect()->route('profile');
+    }
+    public function login(LoginRequest $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->route('profile');
+        } else {
+            return back()->withErrors(['email' => 'invalid credentials']);
+        }
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
